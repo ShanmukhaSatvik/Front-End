92b44e35c4858c7cbc02e4621fd6051cc974c079
ShanmukhaSatvik <satvikshanmukha27@gmail.com>
2025-07-21 16:19:05 +0530

    Front end

--- educase/src/InputField.js
+++
@@ -1,9 +0,0 @@
-function InputField({id,type,placeholder,label,required,style}) {
-    return (
-        <>
-            <label htmlFor={id} className={`label ${id}`} style={style}>{label}{required && <sup className="red">*</sup>}</label>
-            <input type={type} placeholder={placeholder} id={id} className="input"/>
-        </>
-    );
-} 
-export default InputField;
